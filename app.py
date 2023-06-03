24440b9b042e4a036387ff65ec2e63db3f64d516
Yoda-Flash <yaddle.yoda.flash@gmail.com>
2023-06-03 01:15:43 -0700

    Added inputs
    Added first and last name text inputs

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-st.title('Welcome to Portfolio Flash!')
-st.markdown('Here, you can build custom portfolios in a flash.')
+st.title("Welcome to Portfolio Flash!")
+st.markdown("Here, you can build custom portfolios in a flash.")
+st.write(" ")
+firstName = st.text_input(label("What is your first name?"), "First name")
+lastName = st.text_input(label("What is your last name?"), "Last name")
