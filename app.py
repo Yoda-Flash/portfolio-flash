9fa7636f02e3b075594d53e25f7045137811c534
Yoda-Flash <yaddle.yoda.flash@gmail.com>
2023-06-04 02:13:13 -0700

    Finished inputs
    Finished all inputs for career portfolio generation, and organized more files

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Welcome to Portfolio Flash!")
-st.markdown("Here, you can build custom portfolios in a flash.")
-st.write(" ")
-firstName = st.text_input("What is your first name?", )
-lastName = st.text_input("What is your last name?", )
