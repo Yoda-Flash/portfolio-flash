7d61e7691b41b7b971703af32a244c0c7cd702da
Brandon <25nguyenb26@stu.smuhsd.org>
2023-06-03 23:45:58 -0700

    basics for backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 import { Client } from 'appwrite';
+const express = require('express');
+
 
 const client = new Client();
 
+const app = express()
+
+// Pretty indenting
+app.set("json spaces", 2)
+
+app.get('/', (req, res) => res.send("Portfolio-Flash!"))
+
+app.get('/createUser', (req,res) => {
+    
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('647a9e045ef4f5948a16');
+.setEndpoint('https://cloud.appwrite.io/v1')
+.setProject('647a9e045ef4f5948a16');
+
+})
